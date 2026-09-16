a9faddd186aa4afc8fb18a16b01ea218f874d923
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-09-16 23:17:40 +0530

    feat: add course data model

---
+++ Backend/models/Course.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    capacity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    enrollmentDeadline: {
+      type: Date,
+      required: true,
+    },
+
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Course = mongoose.model("Course", courseSchema);
+
+export default Course;
